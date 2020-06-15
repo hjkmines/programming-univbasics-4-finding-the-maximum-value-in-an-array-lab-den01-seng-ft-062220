483a05d29ea43057aa80ed58bd8831c91495069f
Hyung Kim <hjkmines@gmail.com>
2020-06-15 22:30:25 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -5,3 +5,20 @@ def find_max_value(array)
   
   
 end
+
+def find_max (array)
+  
+  counter = 0 
+  
+  max_value = -1
+  
+  while counter < array.length do
+    if max_value < array[counter]
+      max_value = array[counter]
+    end 
+    counter +=-1
+  end
+  
+  max_value
+  
+end
